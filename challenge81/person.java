f0f79918cd8ddd7680fe1c1cccee88a99fbe347e
wolverine <145854700+heetnandu01@users.noreply.github.com>
2025-01-01 02:55:17 +0530

    Add files via upload

---
+++ challenge81/person.java
@@ -0,0 +1,55 @@
+package challenge81;
+/*
+create a class person with attributes name and age.override equals() to compare person objects based on their attributes.override hashcode() consistent with the defination of equals()
+ */
+public class person {
+    private String name;
+    private int age;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + age;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        person other = (person) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (age != other.age)
+            return false;
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    @Override
+    public String toString() {
+        return "person [name=" + name + ", age=" + age + "]";
+    }
+
+     
+}
