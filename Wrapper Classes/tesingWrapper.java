d662e43d0498031017063bf495c6bc1dfcd8dc10
wolverine <145854700+heetnandu01@users.noreply.github.com>
2025-01-05 22:10:41 +0530

    Add files via upload

---
+++ Wrapper Classes/tesingWrapper.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class tesingWrapper {
+    public static void main(String[] args) {
+        Integer first = 55; // this is also called autoboxing 
+        System.out.println(first);
+        int second = first; 
+        // int a = null;
+        /*
+        In normal int class you cannot put null you have to put some value  
+         */
+    }
+}
