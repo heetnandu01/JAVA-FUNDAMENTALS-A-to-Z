68d72cb91d056c483156905931b44066c740f0a7
wolverine <145854700+heetnandu01@users.noreply.github.com>
2025-01-10 21:01:03 +0530

    Add files via upload

---
+++ enums/grade.java
@@ -0,0 +1,6 @@
+package enums;
+
+public enum grade {
+    
+    A,B,C,D,E;
+}
---
+++ enums/testingEnums.java
@@ -0,0 +1,9 @@
+package enums;
+public class testingEnums {
+    public static void main(String[] args) {
+         trafficLight color = trafficLight.RED;
+         color = trafficLight.GREEN;
+
+         grade grade = enums.grade.A;
+    }
+}
